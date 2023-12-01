cefbdc8741eb7e9c0ca42c1477940fd825db6de0
Felix Lampe <mail@flampe.de>
2023-12-01 17:06:16 +0100

    Day 1 part 2

--- day1/day1.exs
+++ day1/day1.exs
@@ -999,12 +999,28 @@ zdsnznxmrbljz7lvdvx74
 qbfvpnxsix3four1lfone
 9one9pjtnncsqzhcszp5"
 
+digits_for_words = %{
+  "one" => "1",
+  "two" => "2",
+  "three" => "3",
+  "four" => "4",
+  "five" => "5",
+  "six" => "6",
+  "seven" => "7",
+  "eight" => "8",
+  "nine" => "9"
+}
+
 lines = String.split(input)
 
 result = Enum.reduce(lines, 0, fn line, sum ->
-  [_, first] = Regex.run(~r/^.*?(\d).*$/, line)
-  [_, last] = Regex.run(~r/^.*(\d).*?$/, line)
-  sum + String.to_integer(first <> last)
+  [_, first] = Regex.run(~r/^.*?(\d|one|two|three|four|five|six|seven|eight|nine).*$/, line)
+  [_, last] = Regex.run(~r/^.*(\d|one|two|three|four|five|six|seven|eight|nine).*?$/, line)
+
+  first_as_digit_string = Map.get(digits_for_words, first, first)
+  last_as_digit_string = Map.get(digits_for_words, last, last)
+
+  sum + String.to_integer(first_as_digit_string <> last_as_digit_string)
 end)
 
 IO.puts(result)
